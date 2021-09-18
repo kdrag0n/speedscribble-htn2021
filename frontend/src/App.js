e757bafd852cc381686e4925a3f0735cefd736ac
Danny Lin <danny@kdrag0n.dev>
2021-09-18 00:26:03 -0700

    frontend: Add simple canvas drawing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import CanvasDraw from 'react-canvas-draw';
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <CanvasDraw />
     </div>
   );
 }
