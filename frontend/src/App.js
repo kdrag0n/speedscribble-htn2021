6ab20f46ec9dd6681dafebcd2934174862c6e085
destruc7i0n <destruc7i0n@users.noreply.github.com>
2021-09-19 05:40:07 -0400

    frontend: hide logo on landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { SWRConfig } from 'swr'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, useRouteMatch } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Play from './pages/Play'
@@ -11,6 +11,23 @@ import { fetcher } from './util'
 
 import { ReactComponent as Logo } from './assets/logo.svg'
 
+const Header = () => {
+  const match = useRouteMatch(['/'])
+
+  const show = !(match && match.isExact)
+
+  return (
+    <div className='p-4 flex items-center'>
+      {show && (
+        <>
+          <Logo className='text-indigo-400 fill-current' width='50' height='60' />
+          <h1 className='font-semibold text-2xl ml-2 text-transparent bg-gradient-to-r from-indigo-400 to-indigo-700 bg-clip-text'>Speed Scribble</h1>
+        </>
+      )}
+    </div>
+  )
+}
+
 function App() {
   // const { token } = useAuthContext()
 
@@ -19,10 +36,7 @@ function App() {
   return (
     <Router>
       <div className='flex flex-col min-h-screen'>
-        <div className='p-4 flex items-center'>
-          <Logo className='text-indigo-400 fill-current' width='50' height='60' />
-          <h1 className='font-semibold text-2xl ml-2 text-transparent bg-gradient-to-r from-indigo-400 to-indigo-700 bg-clip-text'>Speed Scribble</h1>
-        </div>
+        <Header />
         <div className='flex-grow max-w-7xl mx-auto px-4 sm:px-6 grid place-items-center'>
           <SWRConfig value={{ fetcher }}>
             <GameContextProvider>
