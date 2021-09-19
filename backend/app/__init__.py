7d233bced120ce1505fce7dd5d9252583c488316
Danny Lin <danny@kdrag0n.dev>
2021-09-19 01:00:57 -0700

    backend: Update base URLs for prod

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,4 @@
+import platform
 from pathlib import Path
 
 from flask import Flask
@@ -7,12 +8,14 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dragon@localhost:5432/htndraw'
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'cockroachdb://root@localhost:26257/htndraw'
-    app.config['FRONTEND_BASE_URL'] = 'http://localhost:3000'
-    #app.config['API_BASE_URL'] = 'http://localhost:5000'
-    app.config['API_BASE_URL'] = 'https://billy-moscow-empirical-particularly.trycloudflare.com'
+    if platform.node() == 'htn21':
+        app.config['FRONTEND_BASE_URL'] = 'https://speedscribble.tech'
+        app.config['API_BASE_URL'] = 'https://api.speedscribble.tech'
+    else:
+        app.config['FRONTEND_BASE_URL'] = 'http://localhost:3000'
+        #app.config['API_BASE_URL'] = 'http://localhost:5000'
+        app.config['API_BASE_URL'] = 'https://billy-moscow-empirical-particularly.trycloudflare.com'
     app.config['IMAGES_DIR'] = f'{Path().absolute()}/images'
 
     db.init_app(app)
